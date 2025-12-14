7ac3a97047c89c6b9cc30799609ffc0882346d40
Kushagra Tiwari <kushagratiwari24@gmail.com>
2025-12-14 15:37:32 +0530

    generate outline

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -2,5 +2,11 @@
 from src.api.routes.upload import router as upload_router
 from src.api.routes.generation import router as generation_router
 from src.api.routes.download import router as download_router
+from src.api.routes.outline_chat import router as outline_chat_router
 
-__all__ = ["upload_router", "generation_router", "download_router"]
+__all__ = [
+    "upload_router",
+    "generation_router",
+    "download_router",
+    "outline_chat_router",
+]
